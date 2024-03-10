f8dd9852300f672a6461c7af83928638d291a818
Zhong Tao <ztao084@uottawa.ca>
2024-03-10 16:46:05 -0400

    new changes

---
+++ CSI3140A5/javascript/13.3.js
@@ -0,0 +1,40 @@
+/* Group of 36 */
+/* Zhong Tao 300222579 */
+/* Fengshou Xu 300036335 */
+
+function createCanvas() {
+    var side = 100;
+    var tableBody = document.getElementById('tablebody');
+    for (var i = 0; i < side; ++i) {
+        var row = document.createElement("tr");
+
+        for (var j = 0; j < side; ++j) {
+
+            var cell = document.createElement("td");
+            row.appendChild(cell);
+  
+        }
+        tableBody.appendChild(row);
+    }
+    document.getElementById("canvas").addEventListener(
+        "mousemove", processMouseMove, false);
+}
+
+function processMouseMove(e){
+    if(e.target.tagName.toLowerCase() == "td"){
+        if(e.ctrlKey){
+            e.target.setAttribute("class","blue");
+        }
+
+        if(e.shiftKey){
+            e.target.setAttribute("class","red");
+        }
+
+        if(e.altKey){
+            e.target.setAttribute("class","white");
+        }
+    }
+}
+
+ //初始化画布
+window.addEventListener("load", createCanvas, false);
--- CSI3140lab5/javascript/12.3.js
+++ CSI3140lab5/javascript/12.3.js
@@ -3,12 +3,12 @@
 /* Fengshou Xu 300036335 */
 
 window.onload = function() {
-    highlightLinks('yellow'); // 举例用黄色作为背景高亮显示
+    highlightLinks('yellow'); 
 };
 
 function highlightLinks(color) {
-    var links = document.querySelectorAll('a'); // 获取所有链接
+    var links = document.querySelectorAll('a'); 
     for (var i = 0; i < links.length; i++) {
-        links[i].style.backgroundColor = color; // 设置链接背景色
+        links[i].style.backgroundColor = color; 
     }
 }
