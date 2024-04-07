4fc40ee9f87bb42346a27094ba42eb614acbad11
Fengshou <fxu045@uottawa.ca>
2024-04-07 19:35:40 -0400

    update A8

--- CSI3140A8/php/states.php
+++ CSI3140A8/php/states.php
@@ -1,50 +1,56 @@
 <?php
 
-$states = ["Mississippi", "Alabama", "Texas", "Massachusetts", "Kansas"];
+// Group of 36
+// Zhong Tao 300222579
+// Fengshou Xu 300036335
+
+$states = "Mississippi Alabama Texas Massachusetts Kansas";
 
 $statesArray = array();
 
-// a) 寻找以xas结尾的单词
-foreach ($states as $state) {
-    if (preg_match('/xas$/', $state)) {
-        $statesArray[0] = $state;
+// a) find the states that ends with xas
+$states1 = explode(' ', $states);
+foreach ($states1 as $state) {
+    if (str_ends_with($state, 'xas')) {
+        $statesArray[0] = ($state);
         break;
     }
 }
 
-// b) 寻找以k开始，以s结尾的单词（不区分大小写）
-foreach ($states as $state) {
-    if (preg_match('/^k.*s$/i', $state)) {
-        $statesArray[1] = $state;
+// b) find the states that starts with M and ends with s
+foreach ($states1 as $state) {
+    if (str_starts_with($state, 'K') && str_ends_with($state, 's')) {
+        $statesArray[1] = ($state);
         break;
     }
 }
 
-// c) 寻找以M开始，以s结尾的单词
-foreach ($states as $state) {
-    if (preg_match('/^M.*s$/', $state)) {
-        $statesArray[2] = $state;
+// c) find the states that starts with M and ends with s
+foreach ($states1 as $state) {
+    if (str_starts_with($state, 'M') && str_ends_with($state, 's')) {
+        $statesArray[2] = ($state);
         break;
     }
 }
 
-// d) 寻找以a结尾的单词
-foreach ($states as $state) {
-    if (preg_match('/a$/', $state)) {
-        $statesArray[3] = $state;
+// d) find the states that ends with a
+foreach ($states1 as $state) {
+    if (str_ends_with($state, 'a')) {
+        $statesArray[3] = ($state);
         break;
     }
 }
 
-// e) 在字符串开头寻找以M开始的单词
-foreach ($states as $state) {
-    if (preg_match('/^M/', $state)) {
-        $statesArray[4] = $state;
+// e) find the states that starts with M
+foreach ($states1 as $state) {
+    if (str_starts_with($state, 'M')) {
+        $statesArray[4] = ($state);
         break;
     }
 }
 
-// f) 输出数组$statesArray到屏幕
+
+// f) output the result
 echo "<pre>";
 print_r($statesArray);
 echo "</pre>";
